1736f78b45c8520e78b333256c8641038a4a0a28
JB <bradburyjesse7@gmail.com>
2024-01-04 23:40:18 -0700

    Added test for triangle, passes.

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,6 +1,15 @@
 // This is where we will validate our shapes functions. 
+const { Triangle } = require('../lib/shapes.js')
 
 
-const shape = new Triangle();
-shape.setColor("blue");
-expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+
+describe('Shapes', () => {
+
+    describe('Triangle', () => {
+        it("Should render a triangle", () => {
+            const shape = new Triangle();
+            shape.setColor("blue");
+            expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+        });
+    });
+});
